448c77bd82fb55db3228eb6142af56d02212a74c
pw10thu <pw10thu@eduvaud.ch>
2025-08-22 09:25:17 +0200

    Feat: creation et test du fichier express

---
+++ test1_express/app.js
@@ -0,0 +1,11 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello API!');
+});
+
+module.exports = app;
